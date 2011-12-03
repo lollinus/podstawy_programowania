a7e994859c97b917222a78b78d259181dd9134ef
Karol Barski <karol.barski.teb@gmail.com>
2011-12-03 11:12:25 +0100

    Poprawione formatowanie

--- zajecia1/program1.pas
+++ zajecia1/program1.pas
@@ -4,6 +4,6 @@ program Pole_kola;
 
 begin
    writeln('Program oblicza pole kola');
-   write('Pole =', Pi * 5 * 5:8:3); (*comment*)
+   write('Pole = ', Pi * 5 * 5:8:3); (* comment *)
    readln;
 end.
