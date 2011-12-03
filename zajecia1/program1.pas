a9b9bb3c5c8284a4547c9c5bc30282307e6be957
Karol Barski <karol.barski.teb@gmail.com>
2011-12-03 11:31:52 +0100

    Add makefile modify do read radius from keyboard

--- zajecia1/program1.pas
+++ zajecia1/program1.pas
@@ -1,9 +1,14 @@
 program Pole_kola;
-
 { Program oblicza pole ko³a o promieniu 5 }
+{ Program wprowadzany jest z klawiatury }
+
+{ deklaracje zmiennych }
+var r:real; { promien kola - zmienna typu rzeczywistego }
 
 begin
    writeln('Program oblicza pole kola');
-   write('Pole = ', Pi * 5 * 5:8:3); (* comment *)
+   write('Podaj promien kola');
+   readln(r); { odczytaj wartosc z klawiatury i przypisz do podanej zmiennej }
+   write('Pole = ', Pi * r * r:8:3); (* oblicz i wypisz pole kola *)
    readln;
 end.
