3df6507e473d6de7e9aee9e78f7aea72e1cee467
Karol Barski <karol.barski.teb@gmail.com>
2011-12-03 11:11:57 +0100

    Program obliczajacy pole kola

---
+++ zajecia1/program1.pas
@@ -0,0 +1,9 @@
+program Pole_kola;
+
+{ Program oblicza pole ko³a o promieniu 5 }
+
+begin
+   writeln('Program oblicza pole kola');
+   write('Pole =', Pi * 5 * 5:8:3); (*comment*)
+   readln;
+end.
